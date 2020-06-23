d8ff0b8e0bb07efd7d1f8ac10d5d98fab4870632
andres-cox-mojix-com <andreshcox@gmail.com>
2020-06-23 17:47:55 -0300

    restaurant html and css

---
+++ restaurant/main.js
@@ -0,0 +1,9 @@
+//Select element function
+const selectElement = (element) => { return document.querySelector(element) };
+
+const menuToggler = selectElement('.menu-toggle');
+const body = selectElement('body');
+
+menuToggler.addEventListener('click', () => {
+    body.classList.toggle('open');
+});
